ce5af0d7c55a03e98d0a71a83ae7586d9b0175ad
RodrigoMaffezza <rodrigo.maffezza@gmail.com>
2025-05-14 20:52:12 -0300

    feat: add logout button

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -2,6 +2,7 @@ import { Component, output } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-toolbar',
@@ -11,6 +12,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatMenuModule,
   ]
 })
 export class ToolbarComponent {
