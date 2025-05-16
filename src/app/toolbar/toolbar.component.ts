9a2ba7d633a60500f23271bc4201068e791b18fd
Suelen Zierke <suelen.zierke@gmail.com>
2025-05-15 21:37:29 -0300

    feat: adiciona botão de alternância de tema com ThemeService

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -3,6 +3,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { ThemeService } from '../theme/theme.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -17,4 +18,11 @@ import { MatMenuModule } from '@angular/material/menu';
 })
 export class ToolbarComponent {
   readonly menuClick = output();
+  temaAtivo = false;
+
+   constructor(public themeService: ThemeService) {}
+
+  alternarTema(): void {
+    this.themeService.toggleDarkMode();
+  }
 }
