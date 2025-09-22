fc224304ec6a7b53f26df2ec14ecb1fb6843d276
Anthony Muller <anthonyvsmuller@gmail.com>
2025-09-22 20:43:29 -0300

    chore: add confirm dialog

---
+++ src/app/ui/components/confirm-dialog/confirm-dialog.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+
+export interface ConfirmDialogData {
+  title: string;
+  content: string;
+  confirmLabel: string;
+  hideConfirm: boolean;
+  cancelLabel: string;
+  hideCancel: boolean;
+}
+
+@Component({
+  selector: 'app-confirm-dialog',
+  imports: [MatDialogModule, MatButtonModule],
+  templateUrl: './confirm-dialog.html',
+  styleUrl: './confirm-dialog.scss'
+})
+export class ConfirmDialog {
+  readonly data = inject<ConfirmDialogData>(MAT_DIALOG_DATA);
+}
