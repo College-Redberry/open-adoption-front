e4ac7c1ba9c3b5914e1ae19533b9c82747f364fd
Anthony Muller <anthonyvsmuller@gmail.com>
2025-09-22 20:39:15 -0300

    refactor: update file names on main files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { ApplicationConfig, EnvironmentProviders, makeEnvironmentProviders, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { RefreshInterceptor } from './infra/interceptor/refresh';
+import { AuthInterceptor } from './infra/interceptor/auth';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+const provideMaterialConfig = (): EnvironmentProviders => {
+  return makeEnvironmentProviders([
+    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 2500 } },
+  ]);
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withInterceptors([RefreshInterceptor, AuthInterceptor])),
+    provideMaterialConfig(),
+    provideAnimationsAsync(),
+  ]
 };
